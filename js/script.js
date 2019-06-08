99f5bc82f4f832d62d17a777f926b09d2342db17
Tanja Kunz <tk.kunz@gmx.at>
2019-06-08 14:41:10 +0200

    counter function works

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 		
-	$('.pic').attr('src', films[0].pic);
-	$('.name').text(films[0].name);		
-	$('.short').text(films[0].short);		
+	$('.pic').attr('src', data[0].pic);
+	$('.movie').text(data[0].name);		
+	$('.short').text(data[0].short);		
 	// $('.genre').text(films[0].genre);		
 	$('.counter').text(Math.floor((Math.random() * 1000) + 1));
 
 
-	for ( i = 1; i < films.length; i++) {
+	for ( i = 1; i < data.length; i++) {
 		
-		$('.box').clone().addClass('clone').appendTo('#films');
+		$('.box:first').clone().appendTo('#films');
 
 		var img = $('.pic');
-		$(img[i]).attr('src', films[i].pic);
+		$(img[i]).attr('src', data[i].pic);
 
-		var name = $('.name');
-		$(name[i]).text(films[i].name);
+		var movie = $('.movie');
+		$(movie[i]).text(data[i].name);
 
 		var short = $('.short');
-		$(short[i]).text(films[i].short);
+		$(short[i]).text(data[i].short);
 
 		// var genre = $('.genre');
-		// $(genre[i]).text(films[i].genre);
+		// $(genre[i]).text(data[i].genre);
 
 		var counter = $('.counter');
 		$(counter[i]).text(Math.floor((Math.random() * 1000) + 1));
@@ -29,13 +29,39 @@ $(document).ready(function(){
 
 
 
+	// var likeBtn = $('.likeBtn');
 
 
-console.log(img);
-console.log(name);
-console.log(short);
-// console.log(genre);
-console.log(counter);
+	$('.likeBtn').click(function() {
+		var knot = $(this).parents('div.cntBox');
+		var target = knot.find('.counter');
+
+		var cnt = $(target).html();
+			cnt = parseInt(cnt)+1;
+
+		$(target).text(cnt);
+
+	    });
+
+
+	// for ( i = 0; i < likeBtn.length; i++) {
+
+	// 	$(likeBtn[i]).click(function () {
+	// 		var knot = $(this).parents('div.cntBox');
+	// 		var target = knot.find('.counter');
+
+	// 		var cnt = $(target).html();
+	// 			cnt = parseInt(cnt)+1;
+
+	// 		$(target).text(cnt);
+
+	// 	    });
+	// };
+
+
+
+console.log();
+
 });
 
 
